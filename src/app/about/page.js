f93e570d46db0baec4b52d2e4e93b25688995894
kunal passan <Kunalpassan30@gmail.com>
2023-06-06 15:45:12 +0530

    feat: "ui updated"

---
+++ src/app/about/page.js
@@ -0,0 +1,11 @@
+import Herosection from "@/app/components/herosection";
+
+const Page = () => {
+    return (
+        <>
+            <Herosection title ={"Our Story"} imageUrl ={"./about1.svg"}/>
+        </>
+    );
+};
+
+export default Page;
---
+++ src/app/components/Header.js
@@ -0,0 +1,26 @@
+import styles from "@/app/styles/navbar.module.css";
+import Link from "next/link";
+import Nav from "@/app/components/Nav";
+import Image from "next/image";
+
+
+const Header = () => {
+    return (
+        <>
+            <header>
+                <div className={styles.main_header}>
+                    <div className={styles.navbar_brand}>
+                        <Link href="/">
+                            <Image src="/netflix.png" alt="mylogo" width={150} height={140}/>
+                            {/*<Image src="/logo.png" alt="mylogo" width={150} height={40}/>*/}
+                        </Link>
+                    </div>
+                    <Nav/>
+                </div>
+
+            </header>
+        </>
+    );
+};
+
+export default Header;
